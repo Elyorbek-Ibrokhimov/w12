4658ad7f4f47c189ff2bb223f73c75b6c5a2dab9
Elyorbek <shamolkabierkin@gmail.com>
2024-05-29 22:12:02 +0500

    static papka yuklandi

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Region, District
-# Register your models here.
 
-admin.site.register(Region)
-admin.site.register(District)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region')
+    search_fields = ('name',)
+    list_filter = ('region',)
+
+admin.site.register(Region, RegionAdmin)
+admin.site.register(District, DistrictAdmin)
